6bcc77c90a2c887fbbf3a7b5f6b3123426aca8d2
TRUNCATRON <rafaatmetlla@gmail.com>
2025-06-19 21:48:44 -0600

    homogenizacion de idioma y agregar nuevas funciones

---
+++ prueba.py
@@ -0,0 +1,186 @@
+import pygame
+import sys
+import random
+
+pygame.init()
+
+ANCHO, ALTURA = 800, 400
+pantalla = pygame.display.set_mode((ANCHO, ALTURA))
+pygame.display.set_caption("Wagon Wild")
+
+clock = pygame.time.Clock()
+FPS = 60
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (200, 0, 0)
+GREEN = (0, 200, 0)
+GRAY = (180, 180, 180)
+PISO = ALTURA - 50
+TECHO = 195
+
+def mostrar_texto(texto, tamaño, color, x, y, centrado=True):
+    fuente = pygame.font.SysFont(None, tamaño)
+    render = fuente.render(texto, True, color)
+    rect = render.get_rect(center=(x, y)) if centrado else render.get_rect(topleft=(x, y))
+    pantalla.blit(render, rect)
+    return rect
+
+def ultimo_obs(obstaculo):
+    if not obstaculo:
+        return 0 
+    return max([obs.x for obs in obstaculo])
+
+dist_min = 120
+
+
+def pantalla_inicio():
+    while True:
+        pantalla.fill(GRAY)
+        mostrar_texto("Wagon Wild", 80, BLACK, ANCHO // 2, ALTURA // 3)
+
+        boton_start = mostrar_texto("Start", 50, RED, ANCHO // 2, ALTURA // 2)
+        boton_exit = mostrar_texto("Exit", 40, RED, ANCHO // 2, ALTURA // 2 + 70)
+
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if evento.type == pygame.MOUSEBUTTONDOWN:
+                if boton_start.collidepoint(evento.pos):
+                    return  # Inicia juego
+                if boton_exit.collidepoint(evento.pos):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.flip()
+        clock.tick(30)
+
+def pantalla_fin():
+    while True:
+        pantalla.fill(GRAY)
+        mostrar_texto("You Lose", 80, RED, ANCHO // 2, ALTURA // 3)
+
+        boton_reiniciar = mostrar_texto("Restart", 40, BLACK, ANCHO // 2, ALTURA // 2)
+        boton_salir = mostrar_texto("Exit", 40, BLACK, ANCHO // 2, ALTURA // 2 + 70)
+
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if evento.type == pygame.MOUSEBUTTONDOWN:
+                if boton_reiniciar.collidepoint(evento.pos):
+                    juego()
+                if boton_salir.collidepoint(evento.pos):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.flip()
+        clock.tick(30)
+
+def juego():
+    carro_ancho, carro_altura = 50, 50
+    carro_x = 100
+    carro_y = PISO - carro_altura
+    carro_vel_y = 0
+    salto = False
+    gravedad = 1
+    agachado = False
+
+
+    obstaculos = []
+    obstaculo_ancho = 35
+    obstaculo_alto = 50
+    obstaculo_techo_ancho = 35
+    obstaculo_techo_alto = 120
+    obstaculo_vel = 8
+    spawn_piso = 0
+    spawn_techo = 0
+
+    running = True
+    while running:
+        clock.tick(FPS)
+        pantalla.fill(WHITE)
+
+        # Dibujo techo y piso
+        pygame.draw.line(pantalla, BLACK, (0, PISO), (ANCHO, PISO), 2)
+        pygame.draw.line(pantalla, BLACK, (0, TECHO), (ANCHO, TECHO), 2)
+
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if evento.type == pygame.KEYDOWN:
+                if evento.key == pygame.K_SPACE and not salto:
+                    carro_vel_y = -15
+                    salto = True
+                if evento.key == pygame.K_DOWN:
+                    agachado = True
+            if evento.type == pygame.KEYUP:
+                if evento.key == pygame.K_DOWN:
+                    agachado = False
+
+        carro_vel_y += gravedad
+        carro_y += carro_vel_y
+
+        # Toque con piso
+        if carro_y >= PISO - carro_altura:
+            carro_y = PISO - carro_altura
+            salto = False
+
+        # Col. techo
+        if carro_y <= TECHO:
+            carro_y = TECHO
+            carro_vel_y = 0
+
+
+        # Crea obstaculos piso
+        spawn_piso += 1
+        if spawn_piso > random.randint(50, 100):
+            if ANCHO - ultimo_obs(obstaculos) > dist_min:
+                nuevo_obs_piso = pygame.Rect(ANCHO, PISO - obstaculo_alto, obstaculo_ancho, obstaculo_alto)
+                obstaculos.append(nuevo_obs_piso)
+                spawn_piso = 0
+
+        # Crea obstaculos techo
+        spawn_techo += 1
+        if spawn_techo > random.randint(150, 260):
+            if ANCHO - ultimo_obs(obstaculos) > dist_min:
+                nuevo_obs_techo = pygame.Rect(ANCHO, TECHO, obstaculo_techo_ancho, obstaculo_techo_alto)
+                obstaculos.append(nuevo_obs_techo)
+                spawn_techo = 0
+
+        # Mover obstaculos
+        for obs in obstaculos:
+            obs.x -= obstaculo_vel
+
+        # Elimina obstaclos
+        obstaculos = [obs for obs in obstaculos if obs.x + obstaculo_ancho > 0]
+
+
+        if agachado and not salto:
+            altura_actual = carro_altura // 2
+            y_actual = carro_y + carro_altura // 2
+        else:
+            altura_actual = carro_altura
+            y_actual = carro_y
+        
+        rect_carro = pygame.Rect(carro_x, y_actual, carro_ancho, altura_actual)
+
+        
+        for obs in obstaculos:
+            if rect_carro.colliderect(obs):
+                running = False
+
+        # Se dibuja 'carrito' y obstacsulos
+        pygame.draw.rect(pantalla, RED, (carro_x, y_actual, carro_ancho, altura_actual))
+        for obs in obstaculos:
+            pygame.draw.rect(pantalla, GREEN, obs)
+
+        pygame.display.flip()
+
+    pantalla_fin()
+
+# Inicia desde pant. inicio
+pantalla_inicio()
+juego()
